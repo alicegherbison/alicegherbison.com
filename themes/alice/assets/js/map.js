01bf8a0c4d46f0edd7e46fcf2de942de327cf292
alicegherbison <git@alicegherbison.com>
2021-03-27 10:37:41 +0000

    feat: add country data table

--- themes/alice/assets/js/map.js
+++ themes/alice/assets/js/map.js
@@ -4,13 +4,76 @@ google.charts.load("current", {
 
 google.charts.setOnLoadCallback(drawMap);
 
+const travels = [
+  {
+    flag: "🇫🇷",
+    isPlural: false,
+    value: "France",
+    visited: 1,
+  },
+  {
+    flag: "🇮🇸",
+    isPlural: false,
+    value: "Iceland",
+    visited: 1,
+  },
+  {
+    flag: "🇮🇹",
+    isPlural: false,
+    value: "Italy",
+    visited: 1,
+  },
+  {
+    flag: "🇲🇾",
+    isPlural: false,
+    value: "Malaysia",
+    visited: 1,
+  },
+  {
+    flag: "🇳🇱",
+    isPlural: true,
+    value: "Netherlands",
+    visited: 1,
+  },
+  {
+    flag: "🇵🇭",
+    isPlural: true,
+    value: "Philippines",
+    visited: 0,
+  },
+  {
+    flag: "🇰🇷",
+    isPlural: false,
+    value: "South Korea",
+    visited: 0,
+  },
+  {
+    flag: "🇪🇸",
+    isPlural: false,
+    value: "Spain",
+    visited: 1,
+  },
+  {
+    flag: "🇹🇭",
+    isPlural: false,
+    value: "Thailand",
+    visited: 1,
+  },
+  {
+    flag: "🇬🇧",
+    isPlural: true,
+    value: "United Kingdom",
+    visited: 1,
+  },
+];
+
 function drawMap() {
   const data = google.visualization.arrayToDataTable([
     ["Country", "Visited", { p: { html: true }, role: "tooltip", type: "string" }],
     [
       { v: "United Kingdom", f: "" },
       1,
-      `<h2 class="country"><span class="label">Visited</span>🇬🇧&nbsp;United&nbsp;Kingdom</h2>`,
+      `<h2 class="country"><span class="label">Visited</span><span role="img" aria-label="Flag of the United Kingdom">🇬🇧</span>&nbsp;United&nbsp;Kingdom</h2>`,
     ],
     ["Netherlands", 1, "🇳🇱"],
     ["France", 1, "🇫🇷"],
